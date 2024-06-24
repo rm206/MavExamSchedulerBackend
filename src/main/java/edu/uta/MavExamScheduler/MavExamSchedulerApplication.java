2d542b3a6f89a65a45f271a01c1fcf40352ce6ee
Rishabh <panicpark60@gmail.com>
2024-06-24 02:49:21 -0500

    init commit

---
+++ src/main/java/edu/uta/MavExamScheduler/MavExamSchedulerApplication.java
@@ -0,0 +1,13 @@
+package edu.uta.MavExamScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavExamSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavExamSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/uta/MavExamScheduler/MavExamSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uta.MavExamScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavExamSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
