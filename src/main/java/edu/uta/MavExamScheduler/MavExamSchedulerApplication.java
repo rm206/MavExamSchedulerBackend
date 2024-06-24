13b62f4c1ebc94e38293164ddbb7295b5a295a24
Rishabh <panicpark60@gmail.com>
2024-06-24 16:28:44 -0500

    start supabase integ

--- src/main/java/edu/uta/MavExamScheduler/MavExamSchedulerApplication.java
+++ src/main/java/edu/uta/MavExamScheduler/MavExamSchedulerApplication.java
@@ -1,13 +1,27 @@
 package edu.uta.MavExamScheduler;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MavExamSchedulerApplication {
 
+	@Value("${supabase.url}")
+	private String projectUrl;
+
+	@Value("${supabase.key}")
+	private String projectKey;
+
+
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("supabase.url", dotenv.get("SUPABASE_URL"));
+		System.setProperty("supabase.key", dotenv.get("SUPABASE_KEY"));
 		SpringApplication.run(MavExamSchedulerApplication.class, args);
 	}
-
 }
